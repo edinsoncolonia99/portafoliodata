b8bae2ff36ceaf4bd0c3c608dce5ccf88b20de25
edinson <edinsoncolonia@gmail.com>
2025-02-26 20:39:12 -0500

    Body: estructura de la imagen y del acerca de ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component'; 
 import { FooterComponent } from './shared/footer/footer.component';
+import { AcercadeComponent } from "./pages/home/acercade/acercade.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent], 
+  imports: [RouterOutlet, HomeComponent, FooterComponent, AcercadeComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/pages/home/acercade/acercade.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acercade',
+  standalone: true,
+  templateUrl: './acercade.component.html',
+  styleUrl: './acercade.component.scss'
+})
+export class AcercadeComponent {
+
+}
